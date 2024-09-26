b89e4179b4d26814012a29316c76c14cc30c7181
mudan <myudanov223@gmail.com>
2024-09-26 18:03:57 +0300

    Added printing with letters

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
+    static List<Character>listOfLetters = List.of(
+            'A','B','C','D','E','F','G','H','I','J'
+    );
     public static void main(String[] args) {
         ShipsGenerator shipsGenerator = new ShipsGenerator();
         char[][]battlefield = new char[10][10];
@@ -17,7 +21,13 @@ public class Main {
             shipsGenerator.arrangeShips(battlefield,3);
         }
         shipsGenerator.arrangeShips(battlefield,4);
+        System.out.print(" |");
+        for(Character letter:listOfLetters){
+            System.out.print(letter+"|");
+        }
+        System.out.println();
         for(int i=0;i<10;i++){
+            System.out.print(i+" ");
             for(int j=0;j<10;j++){
                 System.out.print(battlefield[i][j]+"|");
             }
